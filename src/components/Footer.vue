ef0a7bba103ee8f2bdd2869fa8bfa4ce4d8ea562
Jet Albano <skyjeth1371120@gmail.com>
2022-04-25 17:19:34 +0800

    Added site logo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,66 +1,70 @@
 <template>
-  <div class="bg-navy-blue-1 row q-pa-xl q-mt-xl text-faded-grey-2">
-    <div class="col">
-      <Container class="full-height">
-        <div class="row">
-          <div class="col-12 col-md-3 flex flex-center">
-            <h1><u>JGA</u></h1>
-          </div>
+  <div>
+    <q-img src="ocean.svg" style="min-height: 200px" />
 
-          <div class="col-12 col-md-3 q-mt-xl">
-            <div class="text-h4 text-center font-pangolin q-mb-md text-weight-bolder">
-              Explore
+    <div class="bg-navy-blue-1 row q-px-xl q-pb-xl text-faded-grey-2">
+      <div class="col">
+        <Container class="full-height">
+          <div class="row">
+            <div class="col-12 col-md-3 flex flex-center b3-red">
+              <q-img src="logos/jet-logo-1.svg" />
             </div>
-            <div
-              class="row flex-center"
-              v-for="section, index in sections"
-              :key="index"
-            >
-              <q-btn
-                @click="scrollTo(section.to)"
-                :label="section.name"
-                class="col-8 q-my-xs"
-                padding="none"
-                size="lg"
-                rounded
-                no-caps
-                flat
-              />
-            </div>
-          </div>
 
-          <div class="col-12 col-md-3 q-mt-xl">
-            <div class="text-h4 text-center font-pangolin q-mb-md text-weight-bolder">
-              Follow
+            <div class="col-12 col-md-3 q-mt-xl">
+              <div class="text-h4 text-center font-pangolin q-mb-md text-weight-bolder">
+                Explore
+              </div>
+              <div
+                class="row flex-center"
+                v-for="section, index in sections"
+                :key="index"
+              >
+                <q-btn
+                  @click="scrollTo(section.to)"
+                  :label="section.name"
+                  class="col-8 q-my-xs"
+                  padding="none"
+                  size="lg"
+                  rounded
+                  no-caps
+                  flat
+                />
+              </div>
             </div>
-            <div
-              class="row flex-center"
-              v-for="social, index in socials"
-              :key="index"
-            >
-              <q-btn
-                :label="social.name"
-                class="col-8 q-my-xs"
-                :href="social.url"
-                target="_blank"
-                padding="none"
-                size="lg"
-                rounded
-                no-caps
-                flat
-              />
-            </div>
-          </div>
 
-          <div class="col-12 col-md-3 q-mt-xl">
-            <div class="text-h4 text-center font-pangolin q-mb-md text-weight-bolder">
-              Contacts
+            <div class="col-12 col-md-3 q-mt-xl">
+              <div class="text-h4 text-center font-pangolin q-mb-md text-weight-bolder">
+                Follow
+              </div>
+              <div
+                class="row flex-center"
+                v-for="social, index in socials"
+                :key="index"
+              >
+                <q-btn
+                  :label="social.name"
+                  class="col-8 q-my-xs"
+                  :href="social.url"
+                  target="_blank"
+                  padding="none"
+                  size="lg"
+                  rounded
+                  no-caps
+                  flat
+                />
+              </div>
             </div>
-            <div class="row flex-center text-h6">skyjeth137120@gmail.com</div>
-            <div class="row flex-center text-h6">+639-75-463-6841</div>
+
+            <div class="col-12 col-md-3 q-mt-xl">
+              <div class="text-h4 text-center font-pangolin q-mb-md text-weight-bolder">
+                Contacts
+              </div>
+              <div class="row flex-center text-h6">skyjeth137120@gmail.com</div>
+              <div class="row flex-center text-h6">+639-75-463-6841</div>
+            </div>
           </div>
-        </div>
-      </Container>
+        </Container>
+      </div>
     </div>
   </div>
 </template>
